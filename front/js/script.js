8978f82ef6d30ba9a0e1a2afc44e0cb866ea217c
AdrienBriaux <devaccess@hotmail.com>
2022-01-26 12:08:04 +0100

    work in progress

--- front/js/script.js
+++ front/js/script.js
@@ -18,33 +18,29 @@ fetch("http://localhost:3000/api/products")
 
 function displayData(productsList) {
 
-    for (let article in productsList) {
+        for (let article in productsList) {
 
-        let productLink = document.createElement("a");
-        document.getElementById('items').appendChild(productLink);
-        productLink.href = `product.html?id=${productsList[article]._id}`;
+            let productLink = document.createElement("a");
+            document.getElementById('items').appendChild(productLink);
+            productLink.href = `product.html?id=${productsList[article]._id}`;
 
-        let productArticle = document.createElement('article');
-        productLink.appendChild(productArticle);
+            let productArticle = document.createElement('article');
+            productLink.appendChild(productArticle);
 
-        let productImg = document.createElement('img');
-        productArticle.appendChild(productImg);
-        productImg.src = productsList[article].imageUrl;
-        productImg.alt = productsList[article].altTxt;
+            let productImg = document.createElement('img');
+            productArticle.appendChild(productImg);
+            productImg.src = productsList[article].imageUrl;
+            productImg.alt = productsList[article].altTxt;
 
-        let productName = document.createElement('h3')
-        productArticle.appendChild(productName);
-        productName.textContent = productsList[article].name;
+            let productName = document.createElement('h3')
+            productArticle.appendChild(productName);
+            productName.textContent = productsList[article].name;
 
-        let productDescription = document.createElement('p');
-        productArticle.appendChild(productDescription);
-        productDescription.textContent = productsList[article].description;
+            let productDescription = document.createElement('p');
+            productArticle.appendChild(productDescription);
+            productDescription.textContent = productsList[article].description;
 
+        }
     }
 
-    .catch (function (error) {
-        alert("Une erreur est survenue")
-    }
-}
-
-displayData();
+    displayData();
