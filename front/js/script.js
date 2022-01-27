3d44b1acec6b4105b3195e36ddaf9429eb30c358
AdrienBriaux <devaccess@hotmail.com>
2022-01-27 14:00:15 +0100

    Intégration des produits sur la page index ok

--- front/js/script.js
+++ front/js/script.js
@@ -9,6 +9,7 @@ fetch("http://localhost:3000/api/products")
     })
     .then(function (productsList) {
         console.log(productsList);
+        displayProductList(productsList);
     })
     .catch(function (error) {
         alert("une erreur est survenue")
@@ -16,31 +17,30 @@ fetch("http://localhost:3000/api/products")
 
 //Intégration dynamique des éléments dans le DOM
 
-function displayData(productsList) {
+function displayProductList(productsList) {
 
-        for (let article in productsList) {
+    for (let p in productsList) {
+        const product = productsList[p];
 
-            let productLink = document.createElement("a");
-            document.getElementById('items').appendChild(productLink);
-            productLink.href = `product.html?id=${productsList[article]._id}`;
+        let productLink = document.createElement("a");
+        document.getElementById('items').appendChild(productLink);
+        productLink.href = `product.html?id=${product._id}`;
 
-            let productArticle = document.createElement('article');
-            productLink.appendChild(productArticle);
+        let productArticle = document.createElement('article');
+        productLink.appendChild(productArticle);
 
-            let productImg = document.createElement('img');
-            productArticle.appendChild(productImg);
-            productImg.src = productsList[article].imageUrl;
-            productImg.alt = productsList[article].altTxt;
+        let productImg = document.createElement('img');
+        productArticle.appendChild(productImg);
+        productImg.src = product.imageUrl;
+        productImg.alt = product.altTxt;
 
-            let productName = document.createElement('h3')
-            productArticle.appendChild(productName);
-            productName.textContent = productsList[article].name;
+        let productName = document.createElement('h3')
+        productArticle.appendChild(productName);
+        productName.textContent = product.name;
 
-            let productDescription = document.createElement('p');
-            productArticle.appendChild(productDescription);
-            productDescription.textContent = productsList[article].description;
+        let productDescription = document.createElement('p');
+        productArticle.appendChild(productDescription);
+        productDescription.textContent = product.description;
 
-        }
     }
-
-    displayData();
+}
