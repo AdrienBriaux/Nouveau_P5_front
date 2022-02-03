21e46e463ce4f4295ae8244fd36c8eb322ba9b77
AdrienBriaux <devaccess@hotmail.com>
2022-02-03 17:58:09 +0100

    Récupération des id des produits du panier ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,15 +1,44 @@
-// Récupération des informations du panier
-
-let localStorageArea = JSON.parse(localStorage.getItem('productStorage'));
-
-console.log(localStorageArea);
-
-// Affichage des produits dans la page panier
-
-function displayCart() {
-
-    for (let product in productStorage) {
+const idCarts = [];
 
 
-    }
-}
+// On récupére l'id de tous les produits du panier
+
+function getCartById() {
+
+    let localStorageArea = JSON.parse(localStorage.getItem('productStorage'));
+
+    if (localStorageArea) {
+
+        for (let id of localStorageArea) {
+
+            let idCart = id.productId;
+            idCarts.push(idCart);
+        }
+    };
+
+    console.log(idCarts)
+};
+
+getCartById();
+
+// Affichage dynamique des produits
+
+
+/* for (let id in idCarts) {
+    fetch("http://localhost:3000/api/products/" + idCart)
+
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+
+        .then(function (product) {
+            console.log(product);
+            displayProduct(product);
+        })
+
+        .catch(function (error) {
+            alert("Une erreur est survenue")
+        });
+}; */
