a2984802f49b493639c85ff48c6ee46f44df04ae
AdrienBriaux <devaccess@hotmail.com>
2022-02-04 17:29:46 +0100

    Récupérer les produits du panier dans l'api ok, intégration dynamique des éléments work in progress

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,49 +1,48 @@
-const idCarts = [];
 let localStorageArea = JSON.parse(localStorage.getItem('productStorage'));
-
-// On récupére l'id de tous les produits du panier
-
-function getIdCart() {
-
-
-
-    if (localStorageArea) {
-
-        for (let id of localStorageArea) {
-
-            let idCart = id.productId;
-            idCarts.push(idCart);
-        }
-    };
-
-    console.log(idCarts)
-};
-
-getIdCart();
 
 // Récupération des données des produits du panier par leur id
 
-function getProductCard() {
 
+async function getProductFromApi(productId) {
+    const response = await fetch("http://localhost:3000/api/products/" + productId);
+    const json = await response.json();
+    return json;
+}
 
-    idCarts.forEach(productId => fetch("http://localhost:3000/api/products/" + productId)
+// Pour chaque identifiants id on lance la fonction getProductFromApi
+// On place la réponse de l'api dans un tableau  
 
+async function getProductCard() {
+    const productList = [];
 
-        .then(function (res) {
-            if (res.ok) {
-                return res.json();
-            }
-        })
+    for (const p in localStorageArea) {
+        const product = localStorageArea[p];
+        const productFromApi = await getProductFromApi(product.productId);
+        productList.push(productFromApi);
+    }
 
-        .then(function (productList) {
-            console.log(productList);
-            displayCart(productList);
-        })
-
-        .catch(function (error) {
-            alert("Une erreur est survenue")
-        })
-    )
+    displayCart(productList);
+    console.log('liste des produits', productList);
 };
 
 getProductCard();
+
+
+// Intégration des produits dans la page
+
+async function displayCart(productList) {
+
+    // Insertion dynamique des éléments
+
+    let cartItemArticle = document.createElement('article');
+    document.getElementById('cart__items').appendChild(cartItemArticle);
+    cartItemArticle.className = 'cart__item';
+
+    let cartItemImgDiv = document.createElement('div');
+    document.querySelector('.cart__item').appendChild(cartItemImgDiv);
+    cartItemImgDiv.className = 'cart__item__img';
+
+    let imgItem = document.createElement('img')
+    imgItem.src = productList.imageUrl;
+    imgItem.alt = productList.alt;
+};
